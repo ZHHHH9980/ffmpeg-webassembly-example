c385851637355f9501a58a8567e6c6f500f83935
Howa <zhonghao@dayuwuxian.com>
2024-05-14 12:12:05 +0800

    feat: test intercept

--- src/mp4info-wrapper.cpp
+++ src/mp4info-wrapper.cpp
@@ -7,50 +7,67 @@
 
 using namespace emscripten;
 
-extern "C"
-{
+extern "C" {
 #include <libavformat/avformat.h>
-};
-
-typedef struct Response
-{
-  std::string format;
-  int duration;
-  int streams;
-} Response;
-
-static AVFormatContext *fmt_ctx;
-
-Response run(std::string filename)
-{
-  // Open the file and read header.
-  int ret;
-  if ((ret = avformat_open_input(&fmt_ctx, filename.c_str(), NULL, NULL)) < 0)
-  {
-    printf("%s", av_err2str(ret));
-  }
-
-  // Read container data.
-  printf("format: %s, duration: %lld us, streams: %d\n",
-         fmt_ctx->iformat->name,
-         fmt_ctx->duration,
-         fmt_ctx->nb_streams);
-
-  // Initialize response struct with format data.
-  Response r = {
-      .format = fmt_ctx->iformat->name,
-      .duration = (int)fmt_ctx->duration,
-      .streams = (int)fmt_ctx->nb_streams,
-  };
-
-  return r;
+#include <libavcodec/avcodec.h>
+#include <libavutil/log.h>
+#include <libavutil/timestamp.h>
 }
 
-EMSCRIPTEN_BINDINGS(structs)
-{
-  emscripten::value_object<Response>("Response")
-      .field("format", &Response::format)
-      .field("duration", &Response::duration)
-      .field("streams", &Response::streams);
-  function("run", &run);
+struct VideoData {
+    std::vector<uint8_t> data;
+
+    VideoData(int size) : data(size) {}
+    size_t getSize() const { return data.size(); }
+};
+
+VideoData cut_video(double start_seconds, double end_seconds, const std::string& inputFileName) {
+    AVFormatContext *inputfile = nullptr, *outputfile = nullptr;
+    AVPacket pkt;
+    av_log_set_level(AV_LOG_INFO);
+
+    if (avformat_open_input(&inputfile, inputFileName.c_str(), nullptr, nullptr) < 0) {
+        EM_ASM({ console.error('Could not open source file'); });
+        return VideoData(0);
+    }
+
+    if (avformat_find_stream_info(inputfile, nullptr) < 0) {
+        avformat_close_input(&inputfile);
+        EM_ASM({ console.error('Could not find stream info'); });
+        return VideoData(0);
+    }
+
+    avformat_alloc_output_context2(&outputfile, nullptr, "mp4", nullptr);
+    // Additional setup for output streams, similar to your existing C code...
+
+    // Assume we have a buffer to write to
+    AVIOContext *avio_ctx = nullptr;
+    uint8_t *avio_ctx_buffer = static_cast<uint8_t*>(av_malloc(avio_ctx_buffer_size));
+    avio_ctx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size, 1, nullptr, nullptr, nullptr, nullptr);
+
+    outputfile->pb = avio_ctx;
+    avformat_write_header(outputfile, nullptr);
+
+    // Read frames, process them and write them to output format as shown in your existing C code...
+
+    av_write_trailer(outputfile);
+
+    // Copy the data to the buffer that will be returned
+    VideoData videoData(avio_ctx->pos);
+    std::copy(avio_ctx_buffer, avio_ctx_buffer + avio_ctx->pos, videoData.data.begin());
+
+    av_free(avio_ctx_buffer);
+    avio_context_free(&avio_ctx);
+    avformat_free_context(outputfile);
+    avformat_close_input(&inputfile);
+
+    return videoData;
+}
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    function("cut_video", &cut_video);
+    class_<VideoData>("VideoData")
+        .constructor<int>()
+        .function("getSize", &VideoData::getSize)
+        .property("data", &VideoData::data);
 }
